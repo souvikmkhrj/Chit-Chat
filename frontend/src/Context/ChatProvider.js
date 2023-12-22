2d4a3cac5cc1f3d90ffc72f309a8739cb59e3335
Souvik Mukherjee <117481543+souvikmkhrj@users.noreply.github.com>
2023-12-22 16:57:29 +0530

    added frontend react file

---
+++ frontend/src/Context/ChatProvider.js
@@ -0,0 +1,51 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const ChatContext = createContext();
+
+const ChatProvider = ({ children }) => {
+  const [selectedChat, setSelectedChat] = useState();
+  const [user, setUser] = useState();
+  const [notification, setNotification] = useState([]);
+  const [chats, setChats] = useState();
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const userInfoString = localStorage.getItem("userInfo");
+    if (userInfoString) {
+      try {
+        const userInfo = JSON.parse(userInfoString);
+        setUser(userInfo);
+      } catch (error) {
+        console.log("Error parsing userInfo:", error);
+      }
+    } else {
+      // Redirect to login page if userInfo is not found
+      history.push("/");
+    }
+  }, [history]);
+
+  return (
+    <ChatContext.Provider
+      value={{
+        selectedChat,
+        setSelectedChat,
+        user,
+        setUser,
+        notification,
+        setNotification,
+        chats,
+        setChats,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const ChatState = () => {
+  return useContext(ChatContext);
+};
+
+export default ChatProvider;
---
+++ frontend/src/Pages/Homepage.js
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react'
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login"
+import Signup from "../components/Authentication/Signup"
+import { useHistory } from 'react-router-dom';
+
+
+
+const Homepage = () => {
+  const history=useHistory();
+    useEffect(()=>{
+        const user=JSON.parse(localStorage.getItem('userInfo'))
+        try {
+          if(user){
+            history.push('/chats');
+        }
+        } catch (error) {
+          console.log("HI IDIOT")
+        }
+        
+    },[history]);
+
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans">
+          Chit-Chat
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant='soft-rounded'>
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login/>
+            </TabPanel>
+            <TabPanel>
+              <Signup/>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  )
+}
+
+export default Homepage
